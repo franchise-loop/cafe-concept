4ce159d4c23f963acd703fedc4c8615aadd84f4e
VishalThapaliya <vishal.thapaliya@gmail.com>
2018-11-20 18:41:10 +0100

    first draft

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,7 @@
 class LandingController < ApplicationController
     def bishal
     end
+
+    def bishal2
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'landing/bishal'
+  get 'landing/bishal2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
